e0083414464ed9f742f7715a5326514960656ac1
Stina Steves <stinasteves@Mac.fritz.box>
2026-02-04 21:37:53 +0100

    test

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 
-//test to make a change//
+//test to make a change Testtwo//
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
